332f11e3b1fbb169b328543e409689d93bac884d
Julio Muller <juliodolago@gmail.com>
2021-03-13 23:09:42 -0300

    Configured ESlint in server

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+    amd: true,
+  },
+  extends: '@lacussoft',
+  ignorePatterns: [
+    '!.sequelizerc',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console': 'off',
+    'semi': ['error', 'always'],
+    'semi-style': ['error', 'last'],
+  },
+};
